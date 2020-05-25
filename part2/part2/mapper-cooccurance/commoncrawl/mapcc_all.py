d474cb4de68ae40d5918c612bb8b6512b20ec396
Jayanthks <jayanthks1@gmail.com>
2020-05-25 15:38:57 -0400

    Add files via upload

---
+++ part2/part2/mapper-cooccurance/commoncrawl/mapcc_all.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+import sys, re
+
+# size of the context window
+window = 2
+cc =0
+# input comes from STDIN (standard input)
+fileaz=sys.stdin
+wd_occ = ["amazon","stock","financ","compani","market","big","busi","year","facebook","develop"]
+for line in sys.stdin:
+    #for line in sys.stdin:
+    for i in wd_occ:
+        line = line.strip()
+        line=line.lower()
+    # split the line into words
+        #if i in line:
+        words = line.split()
+        for j in xrange(len(words)-1):
+            #for k in range(i+1,len(words)):
+            if words[j] == i:
+                print ("%s-%s\t%s" % (words[j],words[j+1], 1))
+
+
+# In[ ]:
+
+
+
+
